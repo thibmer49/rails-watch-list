b4472314724c440a63b6cc514a4de9dbe05a19b7
Thibault Mercier <thib.mercier@ymail.com>
2023-02-23 15:33:35 +0100

    Routes, controllers & views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only:[:index, :new, :create, :show] do
-    resources :bookmarks
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
